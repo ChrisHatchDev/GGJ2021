b9d746013bc794141e46a5d9da88da881f5338c2
Chris Hatch <chatch@chris-hatch.com>
2021-01-29 20:59:37 -0500

    More Folders

---
+++ Assets/SoyCharacterController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoyCharacterController : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterController _cc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
