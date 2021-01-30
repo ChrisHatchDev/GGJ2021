a404245da34ca612fd1986e6ae75fca2244a2079
Chris Hatch <chatch@chris-hatch.com>
2021-01-30 01:27:41 -0500

    WIP Character Controller, Final IK Plug In

---
+++ Assets/Normal/Examples/Cube Player/Scripts/PlayerRealtimeManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Normal.Realtime;
+
+public class PlayerRealtimeManager : MonoBehaviour {
+    private Realtime _realtime;
+
+    [SerializeField]
+    private GameObject _playerPrefab;
+
+    private void Awake() {
+        _realtime = GetComponent<Realtime>();
+
+        // Notify us when Realtime successfully connects to the room
+        _realtime.didConnectToRoom += DidConnectToRoom;
+    }
+
+    private void DidConnectToRoom(Realtime realtime) {
+        // Instantiate the CubePlayer for this client once we've successfully connected to the room
+        Realtime.Instantiate(_playerPrefab.name,                 // Prefab name
+                            position: Vector3.up,          // Start 1 meter in the air
+                            rotation: Quaternion.identity, // No rotation
+                        ownedByClient: true,                // Make sure the RealtimeView on this prefab is owned by this client
+            preventOwnershipTakeover: true,                // Prevent other clients from calling RequestOwnership() on the root RealtimeView.
+                            useInstance: realtime);           // Use the instance of Realtime that fired the didConnectToRoom event.
+    }
+}
---
+++ Assets/Scripts/SoyBoySync.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Normal.Realtime;
+
+public class SoyBoySync : RealtimeComponent<PlayerDataModel>
+{
+    public int _type = -1;
+
+    [SerializeField]
+    private MeshRenderer _meshRenderer;
+
+    [SerializeField]
+    private Material _hiderMaterial;
+
+    [SerializeField]
+    private Material _seekerMaterial;
+
+    private void Awake()
+    {
+    }
+    
+    private void PlayerTypeDidChange(PlayerDataModel model, int type)
+    {
+        UpdateType(type);
+    }
+
+    private void UpdateType(int type)
+    {
+        _type = type;
+
+        _meshRenderer.material = _type == 0 ? _hiderMaterial : _seekerMaterial;
+    }
+
+    public void SetPlayerType (int type)
+    {
+        model.playerType = type;
+    }
+
+    protected override void OnRealtimeModelReplaced(PlayerDataModel previousModel, PlayerDataModel currentModel) {
+        if (previousModel != null) {
+            // Unregister from events
+            previousModel.playerTypeDidChange -= PlayerTypeDidChange;
+        }
+        
+        if (currentModel != null) {
+            // If this is a model that has no data set on it, populate it with the current mesh renderer color.
+            if (currentModel.isFreshModel)
+                currentModel.playerType = -1;
+        
+            // Update the mesh render to match the new model
+            UpdateType(-1);
+
+            // Register for events so we'll know if the color changes later
+            currentModel.playerTypeDidChange += PlayerTypeDidChange;
+        }
+    }
+}
